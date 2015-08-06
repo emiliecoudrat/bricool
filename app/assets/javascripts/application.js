691b106c9c5997070e76f1acd665090a1bb9e859
emiliecoudrat <coudrat.emilie@gmail.com>
2015-08-06 13:46:33 +0200

    pas fini

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// app/assets/javascripts/application.js
-//
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
