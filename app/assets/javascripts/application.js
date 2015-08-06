95b32063a897b40b766d19a92cc61db447e9a7e1
emiliecoudrat <coudrat.emilie@gmail.com>
2015-08-06 13:33:28 +0200

    trying to fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// app/assets/javascripts/application.js
+//
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
