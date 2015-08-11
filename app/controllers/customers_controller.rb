9be5ae734aa5f839a428f29393a8c4f55ac57dd8
CandiceSG <candice.salagasperini@gmail.com>
2015-08-11 17:36:40 +0200

    pb d'author non réglé

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,9 @@
 class CustomersController < ApplicationController
   before_filter :authenticate_user!
-  before_action :set_customer, only: [:index, :show, :edit, :update, :destroy]
-  before_action :set_user, only: [:index, :show, :edit, :update, :destroy]
+  # before_filter :ensure_customer, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
-  def show
-  end
 
   def edit
   end
@@ -19,13 +18,21 @@ class CustomersController < ApplicationController
     redirect_to home_index
   end
 
+private
+
   def set_customer
-     @customer = current_user.profileable
+    @customer = current_user.profileable
   end
+
 
   def set_user
       @user = current_user
   end
+
+  # def ensure_customer
+  #   redirect_to(home_index_path) unless @customer.nil?
+  #   raise
+  # end
 
   def customer_params
       params.require(:customer).permit(:first_name, :last_name, :address, :city, :zipcode, :profileable_type, :profileable_id)
