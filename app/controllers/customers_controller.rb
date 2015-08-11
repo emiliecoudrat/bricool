d4332678b7d8a2626d6cd1f2253a215f1f6c6c3e
CandiceSG <candice.salagasperini@gmail.com>
2015-08-11 14:51:40 +0200

    en cours

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
 class CustomersController < ApplicationController
   before_filter :authenticate_user!
+  before_action :set_customer, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_user, only: [:index, :show, :edit, :update, :destroy]
 
   def show
   end
@@ -8,8 +10,24 @@ class CustomersController < ApplicationController
   end
 
   def update
+    @customer.update(customer_params)
+    redirect_to home_index
   end
 
   def destroy
+    @customer.destroy
+    redirect_to home_index
+  end
+
+  def set_customer
+     @customer = current_user.profileable
+  end
+
+  def set_user
+      @user = current_user
+  end
+
+  def customer_params
+      params.require(:customer).permit(:first_name, :last_name, :address, :city, :zipcode, :profileable_type, :profileable_id)
   end
 end
